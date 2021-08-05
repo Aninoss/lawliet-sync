cebf1971b1fb3a77e2550f9ff44b91667a7e3923
Aninoss <leonblech@tutanota.com>
2021-08-06 01:12:48 +0200

    PREMIUM event doesn't include mutual guilds anymore

--- src/main/java/syncserver/events/OnPremium.java
+++ src/main/java/syncserver/events/OnPremium.java
@@ -1,22 +1,16 @@
 package syncserver.events;
 
 import java.util.HashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import core.cache.PatreonCache;
-import core.cache.SingleCache;
 import mysql.modules.premium.DBPremium;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import syncserver.*;
+import syncserver.ClientTypes;
+import syncserver.SyncServerEvent;
+import syncserver.SyncServerFunction;
 
 @SyncServerEvent(event = "PREMIUM")
 public class OnPremium implements SyncServerFunction {
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(SingleCache.class);
 
     @Override
     public JSONObject apply(String socketId, JSONObject jsonObject) {
@@ -29,10 +23,6 @@ public class OnPremium implements SyncServerFunction {
             int tier = PatreonCache.getInstance().getUserTier(userId);
             int n = PatreonCache.tierToPremiumSlotNumber(tier);
 
-            if (n > 0) {
-                jsonGuilds = getMutualGuilds(userId);
-            }
-
             HashMap<Integer, Long> slotMap = DBPremium.fetchForUser(userId);
             for (int i = 0; i < n; i++) {
                 jsonSlots.put(getSlot(slotMap, i));
@@ -44,29 +34,6 @@ public class OnPremium implements SyncServerFunction {
             return jsonResponse;
         }
         return null;
-    }
-
-    private JSONArray getMutualGuilds(long userId) {
-        JSONArray jsonGuilds = new JSONArray();
-        JSONObject jsonUserId = new JSONObject();
-        jsonUserId.put("user_id", userId);
-
-        ClusterConnectionManager.getInstance().getActiveClusters().parallelStream()
-                .forEach(cluster -> {
-                    try {
-                        JSONArray jsonClusterGuilds = SendEvent.sendJSON("MUTUAL_SERVERS", cluster.getClusterId(), jsonUserId)
-                                .get(3, TimeUnit.SECONDS)
-                                .getJSONArray("guilds");
-
-                        for (int i = 0; i < jsonClusterGuilds.length(); i++) {
-                            jsonGuilds.put(jsonClusterGuilds.getJSONObject(i));
-                        }
-                    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                        LOGGER.error("Exceptions on mutual servers", e);
-                    }
-                });
-
-        return jsonGuilds;
     }
 
     private long getSlot(HashMap<Integer, Long> slotMap, int i) {
